fe54e7ccbbccbd4122ce76aa2761925eb6b0504f
sodu <bischoflaurin@gmail.com>
2024-05-27 23:10:32 +0200

    feat(UI) add coordinates to debug label

--- Scripts/TempDiagnosticsPanelScript.gd
+++ Scripts/TempDiagnosticsPanelScript.gd
@@ -1,4 +1,6 @@
 extends Label
 
+@onready var player = $"../Player"
+
 func _process(delta):
-	text = "FPS: " + str(Engine.get_frames_per_second())
+	text = "FPS: " + str(Engine.get_frames_per_second()) + "\n" + "X: " + str(round(player.translation.x)) + "\n" + "Y: " + str(round(player.translation.y)) + "\n" + "Z: " + str(round(player.translation.z))
