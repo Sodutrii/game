94f1fc118189dbfc18ed013b787f2f2be3cc0ff4
Sodutrii <bischoflaurin@gmail.com>
2024-05-29 14:51:41 +0200

    stuff

--- Scripts/TempDiagnosticsPanelScript.gd
+++ Scripts/TempDiagnosticsPanelScript.gd
@@ -3,4 +3,5 @@ extends Label
 @onready var player = $"../Player"
 
 func _process(delta):
-	text = "FPS: " + str(Engine.get_frames_per_second()) + "\n" + "X: " + str(round(player.translation.x)) + "\n" + "Y: " + str(round(player.translation.y)) + "\n" + "Z: " + str(round(player.translation.z))
+	pass
+	#text = "FPS: " + str(Engine.get_frames_per_second()) + "\n" + "X: " + str(round(player.translation.x)) + "\n" + "Y: " + str(round(player.translation.y)) + "\n" + "Z: " + str(round(player.translation.z))
